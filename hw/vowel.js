acf989c331f1654bc8478b8a00e1fef6e09f5908
rldbsgk327 <rldbsgk327@gmail.com>
2020-04-08 01:59:50 +0900

    hw

--- hw/vowel.js
+++ hw/vowel.js
@@ -1,7 +1,7 @@
-let str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
+let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
 
-for(let i=0; i<str.length; i++) {
-	if ( (str[i].search(/[aeiou]/)) != -1 ) {
+for(let i=0; i<s.length; i++) {
+	if ( (s[i].search(/[aeiou]/)) != -1 ) {
 		console.log(i)
 	}
 }
