8be64c1e191ce37ee29def638c54d702dabb5410
rldbsgk327 <rldbsgk327@gmail.com>
2020-04-07 23:50:33 +0900

    hw

--- hw/split.js
+++ hw/split.js
@@ -1,6 +1,7 @@
 let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
-let a = s.split(/[\t , .]+/);
+let a = s.split(" ");
 
 for (let i=0; i<a.length; ++i) {
  console.log(a[i]);
 }
+
