beb66e6632389bcfc4f2f7038b9989142c5a976e
rldbsgk327 <rldbsgk327@gmail.com>
2020-04-07 23:48:05 +0900

    hw

--- hw/split.js
+++ hw/split.js
@@ -1,5 +1,6 @@
 let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
-let a = s.split(" ");
+let a = s.split(/[\t , .]+/);
+
 for (let i=0; i<a.length; ++i) {
  console.log(a[i]);
 }
